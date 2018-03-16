f70754b8367944a615923b067a5730a3d5d59616
Tamás Nagy <nagy.tomi@gmail.com>
2018-03-16 14:04:22 +0100

    refactor: Add aria-label on index page restaurant links.

--- js/main.js
+++ js/main.js
@@ -178,6 +178,8 @@ createRestaurantHTML = restaurant => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.title = `View details about ${restaurant.name}`;
+  more.setAttribute('aria-label', `View details about ${restaurant.name}`);
   divContainer.append(more);
 
   li.append(divContainer);
